7f973ece43dc081f46128ecf031876df7cfe22f6
the-Azrael <tusaev.maxim@yandex.ru>
2022-08-08 19:57:22 +0400

    second task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,8 @@
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-        String[] texts = new String[25];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
-        }
-
-        List<Thread> threads = new ArrayList<>();
-        long startTs = System.currentTimeMillis(); // start time
-
-        for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(thread);
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTs - startTs) + "ms");
-    }
-
+    private static final int SIZE = 25;
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -52,5 +10,59 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static String[] getStrings() {
+        String[] strings = new String[SIZE];
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = generateText("aab", 30_000);
+        }
+        return strings;
+    }
+
+    public static int getMax(String text) {
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        return maxSize;
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newFixedThreadPool(SIZE);
+        List<Future> futures = new ArrayList<>();
+        long startTs = System.currentTimeMillis(); // start time
+        String[] texts = getStrings();
+        for (String text : texts) {
+            Callable<String> myCallable = new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return text.substring(1, 100) + " -> " + getMax(text);
+                }
+            };
+            Future<String> future = es.submit(myCallable);
+            futures.add(future);
+        }
+        for (Future<String> future : futures) {
+            String line = future.get();
+            System.out.println(line);
+        }
+        long endTs = System.currentTimeMillis(); // end time
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        es.shutdown();
     }
 }
